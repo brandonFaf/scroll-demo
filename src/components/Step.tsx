dd9d517ecb6764f3452968489b3839f5b09681aa
Brandon <brandon.myers@zapier.com>
2023-03-15 10:02:39 -0400

    move logic from scrollIntoView into list item

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,5 +1,5 @@
 import { useIsFocusedSearchHighlight } from '@/state/isSearchHighlightFocusedState';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ScrollIntoView from './ScrollIntoView';
 
 type Props = {
@@ -9,10 +9,32 @@ type Props = {
 
 const Step = ({ data, query }: Props) => {
   const isSearchHighlightFocused = useIsFocusedSearchHighlight(data, query);
+  const ref = React.useRef<HTMLLIElement>(null);
+  useEffect(() => {
+    console.log('run effect, shouldScollIntoView', isSearchHighlightFocused);
+    if (isSearchHighlightFocused) {
+      const domNode = document.getElementById(data);
+      const shouldRun =
+        domNode && domNode instanceof Element && domNode.scrollIntoView;
+
+      console.log('shouldRun:', shouldRun);
+      console.log('domNode:', domNode);
+      if (domNode && shouldRun) {
+        // setTimeout(() => {
+        domNode.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        // }, 0);
+      }
+    }
+  }, [isSearchHighlightFocused, ref, data]);
   return (
-    <ScrollIntoView shouldScrollIntoView={isSearchHighlightFocused}>
-      <li style={{ paddingBlock: 25 }}>{data}</li>
-    </ScrollIntoView>
+    // <ScrollIntoView elRef={ref} shouldScrollIntoView={isSearchHighlightFocused}>
+    <li ref={ref} id={data} style={{ paddingBlock: 25 }}>
+      {data}
+    </li>
+    // </ScrollIntoView>
   );
 };
 
